5767d5638ef1340fadac0e0090e25fdfd54eed7b
miltonrubro@gmail.com <miltonrubro@gmail.com>
2020-12-03 23:24:03 -0300

    Adicionando entidade

---
+++ src/br/com/devmos/pacman/model/Entity.java
@@ -0,0 +1,106 @@
+package br.com.devmos.pacman.model;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.xml.soap.Node;
+
+public class Entity {
+	
+	protected double x;
+	protected double y;
+	protected int width;
+	protected int height;
+	public int depth;
+	protected List<Node> path;
+	public boolean debug = false;
+	private BufferedImage sprite;
+	
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+
+	public List<Node> getPath() {
+		return path;
+	}
+
+	public void setPath(List<Node> path) {
+		this.path = path;
+	}
+
+	public BufferedImage getSprite() {
+		return sprite;
+	}
+
+	public void setSprite(BufferedImage sprite) {
+		this.sprite = sprite;
+	}
+
+	public Entity(double x, double y, int width, int height, BufferedImage sprite) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.sprite = sprite;
+	}
+	
+	public static Comparator<Entity> nodeSorter = new Comparator<Entity>() {
+		
+		@Override
+		public int compare(Entity e0, Entity e1) {
+			if(e1.depth < e0.depth)
+				return +1;
+			if(e1.depth > e0.depth)
+				return -1;
+			return 0;
+		}
+	};
+	
+	public void update() {
+		
+	}
+	
+	public void render(Graphics graphics) {
+
+	}
+	
+}
